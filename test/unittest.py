80b7af2609546880524349a5727bc5f66eb5ec65
kksssi <99618361+kksssi@users.noreply.github.com>
2022-02-17 21:43:52 +0200

    added first test draft

--- test/unittest.py
+++ test/unittest.py
@@ -1,7 +1,17 @@
-import unittest
-from parking_bot import runner
-# from unittest import IsolatedAsyncioTestCase
+from parking_bot.flows.create_car_place import *
+from parking_bot.settings import *
+from aiogram import Bot, Dispatcher
+from unittest import IsolatedAsyncioTestCase
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-class TestBot(unittest.TestCase):
-    pass
+class TestBot(IsolatedAsyncioTestCase):
+
+    async def TestStartInput(self):
+        bot = Bot(token=BOT_TOKEN)
+        dp = Dispatcher(bot)
+        flow = CreateCarPlaceFlow(dp)
+        state = FSMContext(chat="", storage=MemoryStorage(), user="")  # chat and user ids?
+        expected_state = NewCarDataState.start_data_input
+        await flow.start_input(input_cb.new(type="start"), state)
+        self.assertEqual(expected_state, NewCarDataState.start_data_input.state)
